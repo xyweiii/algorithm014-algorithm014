4595ce6c8070c905cbb4ce937253b2a1e4ecc8c9
xiangyu.wei <iterxywei@foxmail.com>
2020-08-20 18:20:39 +0800

    字母异位词分组

---
+++ Week_02/GroupAnagrams.java
@@ -0,0 +1,49 @@
+package com.sioo.discover.api.test;
+
+import java.util.*;
+
+/**
+ *
+ * 给定一个字符串数组，将字母异位词组合在一起。字母异位词指字母相同，但排列不同的字符串。
+ *
+ * 示例:
+ *
+ * 输入: ["eat", "tea", "tan", "ate", "nat", "bat"]
+ * 输出:
+ * [
+ *   ["ate","eat","tea"],
+ *   ["nat","tan"],
+ *   ["bat"]
+ * ]
+ * 说明：
+ *
+ * 所有输入均为小写字母。
+ * 不考虑答案输出的顺序。
+ *
+ *
+ * @author : xywei
+ * @date : 2020-08-20
+ */
+public class GroupAnagrams {
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) {
+            return new ArrayList<>();
+        }
+        if (strs.length == 0) return new ArrayList();
+        Map<String, List> ans = new HashMap<String, List>();
+        for (String s : strs) {
+            char[] ca = s.toCharArray();
+            Arrays.sort(ca);
+            String key = String.valueOf(ca);
+            if (!ans.containsKey(key)) ans.put(key, new ArrayList());
+            ans.get(key).add(s);
+        }
+        return new ArrayList(ans.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"eat","tea","tan","ate","nat","bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+}
